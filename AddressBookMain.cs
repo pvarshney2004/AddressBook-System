b4f03df6a1952cb0a274ac00141eed67c2226f3c
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-21 16:48:46 +0530

    [Prashant] add AddressBookMain class to start program

---
+++ AddressBookMain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
